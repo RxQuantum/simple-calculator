e716fed9cb84a02322a7e5c5ef123f820f5d372e
RxQuantum <106014786+RxQuantum@users.noreply.github.com>
2023-03-31 15:56:48 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+function display(value){
+    document.getElementById('result').value += value;
+    return value
+}
+
+function solve(){
+        let equation  = document.getElementById("result").value;
+        let solution = eval(equation);
+        document.getElementById("result2").value = solution;  
+        
+    }
+
+function clearScreen(){
+    document.getElementById("result2").value = "";
+    document.getElementById("result").value = "";
+}
